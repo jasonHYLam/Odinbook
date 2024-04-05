32846a8824fed3b3abcccbf1de5e9c4d379fb4c5
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-05 19:38:49 +0100

    Add Form To Login Page

--- src/components/loginSignup/Login.jsx
+++ src/components/loginSignup/Login.jsx
@@ -1,9 +1,20 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   return (
     <main>
       <p>Login</p>
+      <form onSubmit={handleSubmit()}>
+        <input type="text" {...register("username", { required: true })} />
+        <input type="password" {...register("password", { required: true })} />
+        <input type="submit" value="Login" />
+      </form>
       <Link to="/sign_up">New here? Sign up</Link>
     </main>
   );
