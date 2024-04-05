76b142797e9abe19e16c89b1559bcbf0c592710f
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-05 13:11:02 +0100

    Start Creating Homepage Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  // login
+  //signup
+  //home (feed)
+  // addfriend
+  // createPost
+  // viewOwnProfile
+  // view other profile
+  // view post
+  // logout
+  // {}
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
