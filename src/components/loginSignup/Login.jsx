1ba0294b1a3abce722b90082c82ff90ee7fe3dcf
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-05 22:21:13 +0100

    Handle And Set And Display AuthError

--- src/components/loginSignup/Login.jsx
+++ src/components/loginSignup/Login.jsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { fetchData } from "../../helper/helperUtils";
+import { useState } from "react";
 
 export function Login() {
   const {
@@ -11,14 +12,27 @@ export function Login() {
 
   const navigate = useNavigate();
 
+  const [authError, setAuthError] = useState(null);
+
   async function submitLoginData(data) {
     const dataToSubmit = JSON.stringify(data);
-    const response = await fetchData("auth/login", "POST", dataToSubmit);
-    if (!response.ok || response instanceof Error) {
-      navigate("/error");
+    console.log("checking dataToSubmit");
+    console.log(dataToSubmit);
+    const response = await fetchData("/auth/login", "POST", dataToSubmit);
+    if (response.status === 401) {
+      setAuthError("Incorrect username/password");
+    } else if (!response.ok) {
+      console.log(response);
+      console.log("get it shawty");
     }
-    console.log("will this happen instantly");
-    navigate("/feed");
+    // else if (!response.ok || response instanceof Error) {
+    else if (response instanceof Error) {
+      console.log("something happened");
+      navigate("/error");
+    } else {
+      console.log("will this happen instantly");
+      // navigate("/");
+    }
   }
   return (
     <main>
@@ -27,6 +41,11 @@ export function Login() {
         <input type="text" {...register("username", { required: true })} />
         <input type="password" {...register("password", { required: true })} />
         <input type="submit" value="Login" />
+        <section>
+          {errors.username && <span>Please provide username</span>}
+          {errors.password && <span>Please provide password</span>}
+          {authError ? <span>{authError}</span> : null}
+        </section>
       </form>
       <Link to="/sign_up">New here? Sign up</Link>
     </main>
