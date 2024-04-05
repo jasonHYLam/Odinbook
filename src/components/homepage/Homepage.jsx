d74759a41a522d440318be0dc6b6a5739b4c4f9b
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-05 16:40:25 +0100

    Import Header Into HomePage

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "./Header";
 
 export function Homepage() {
   return (
     <>
-      <header>its me the header</header>
+      <Header />
       <main>
         <p>its me homepage</p>
         <Outlet />
