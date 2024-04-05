af2cf30f9e84e89ea086f0f238a6c633d4173ac2
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-05 14:12:53 +0100

    Start Writing CreatePost Feed Post Components

---
+++ src/components/homepage/createPost/CreatePost.jsx
@@ -0,0 +1,8 @@
+export function CreatePost() {
+  <main>
+    <form action="">
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <input type="submit" value="Post" />
+    </form>
+  </main>;
+}
