e955b818ec3798420dc323a19b43128d1ad8c841
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-06 15:07:16 +0100

    Use React Hook Form For CreatePost Component

--- src/components/homepage/createPost/CreatePost.jsx
+++ src/components/homepage/createPost/CreatePost.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export function CreatePost() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [imagesToUpload, setImagesToUpload] = useState([]);
+  const [text, setText] = useState("");
+
+  function selectImages() {
+    setImagesToUpload(e.target.files);
+  }
+
+  async function post(data) {
+    if (imagesToUpload) return;
+    console.log("checking data");
+    console.log(data);
+  }
+
   return (
     <main>
       <p>it's me createPost</p>
-      <form action="">
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+      <form encType="multipart/form-data" onSubmit={handleSubmit(post)}>
+        <input type="file" multiple onChange={selectImages} />
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          {...register("text", { required: true })}
+        ></textarea>
         <input type="submit" value="Post" />
       </form>
     </main>
