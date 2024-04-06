6390d26b6e12c3136c06d6123322ace258de5844
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-06 14:09:00 +0100

    Set Up UseEffect For UserPosts For PersonalProfile

--- src/components/homepage/personalProfile/PersonalProfile.jsx
+++ src/components/homepage/personalProfile/PersonalProfile.jsx
@@ -1,11 +1,52 @@
-import { useOutletContext } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { fetchData } from "../../../helper/helperUtils";
 
 export function PersonalProfile() {
+  const navigate = useNavigate();
   const { user } = useOutletContext();
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  console.log("checking posts");
+  console.log(posts);
+
+  useEffect(() => {
+    async function getUserPosts() {
+      const getPostsResponse = await fetchData(
+        "user/view_personal_profile",
+        "GET"
+      );
+      if (!getPostsResponse.ok || getPostsResponse instanceof Error) {
+        navigate("/error");
+      } else {
+        const { posts } = await getPostsResponse.json();
+        setPosts(posts);
+        setIsLoading(false);
+      }
+    }
+    getUserPosts();
+  }, []);
   return (
     <main>
       <p>it's me personalProfile</p>
       <h2>{user.username}</h2>
+      <section>
+        <p>Your Posts</p>
+        {isLoading ? (
+          <p>loading</p>
+        ) : (
+          <ul>
+            {posts.map((post) => {
+              return (
+                <>
+                  <p>post.text;</p>
+                </>
+              );
+            })}
+          </ul>
+        )}
+      </section>
     </main>
   );
 }
