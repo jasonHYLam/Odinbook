85f9b224aaae43b5f14188644652f8f729e63a45
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-07 12:20:13 +0100

    Fetch Post Data When Rendering Post Component

--- src/components/homepage/post/Post.jsx
+++ src/components/homepage/post/Post.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import { fetchData } from "../../../helper/helperUtils";
+import { useNavigate, useParams } from "react-router-dom";
+
 export function Post() {
+  const { postID } = useParams();
+  const navigate = useNavigate();
+  const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchPostAndComments() {
+      const getResponse = await fetchData(`post/${postID}`, "GET");
+      if (!getResponse.ok || getResponse instanceof Error) {
+        navigate("/error");
+      } else {
+        const { post, comments } = await getResponse.json();
+        setPost(post);
+        setComments(comments);
+        setIsLoading(false);
+      }
+    }
+
+    fetchPostAndComments();
+  }, []);
+
   return (
     <>
       <p>it's me a post</p>
+      {isLoading ? (
+        <p>loading</p>
+      ) : (
+        <>
+          <main>
+            <section>
+              <p>{post.text}</p>
+            </section>
+          </main>
+        </>
+      )}
     </>
   );
 }
