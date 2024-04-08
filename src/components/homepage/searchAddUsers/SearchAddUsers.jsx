9259eb8858039987e9d7049b86cb7e798e912c16
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-08 13:17:39 +0100

    Display MatchingUsers In SearchAddUsers

--- src/components/homepage/searchAddUsers/SearchAddUsers.jsx
+++ src/components/homepage/searchAddUsers/SearchAddUsers.jsx
@@ -1,7 +1,59 @@
+import { useEffect, useState } from "react";
+import { fetchData } from "../../../helper/helperUtils";
+import { useNavigate } from "react-router-dom";
+
 export function SearchAddUsers() {
+  const navigate = useNavigate();
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [matchingUsers, setMatchingUsers] = useState([]);
+
+  useEffect(() => {
+    async function searchUsers() {
+      if (!searchQuery) return;
+      const dataToSubmit = JSON.stringify({ searchQuery });
+      console.log("dataToSubmit");
+      console.log(dataToSubmit);
+      const searchResponse = await fetchData(
+        `user/search_users`,
+        "POST",
+        dataToSubmit
+      );
+      if (!searchResponse.ok || searchResponse instanceof Error) {
+        navigate("/error");
+      } else {
+        const { users } = await searchResponse.json();
+        console.log(users);
+        setMatchingUsers(users);
+      }
+    }
+    searchUsers();
+  }, [searchQuery]);
+
+  async function followUser(userID) {
+    const followResponse = await fetchData(`user/${userID}/follow`, "POST");
+    if (!followResponse.ok || followResponse instanceof Error) {
+      navigate("/error");
+    } else {
+      return;
+    }
+  }
+
   return (
     <main>
-      <p>it's me searchAddUsers</p>
+      <p>it's me searchaddusers</p>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <section>
+        <ul>
+          {matchingUsers.map((user) => {
+            return <article>{user.username}</article>;
+          })}
+        </ul>
+      </section>
     </main>
   );
 }
