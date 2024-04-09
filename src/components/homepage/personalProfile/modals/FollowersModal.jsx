0c5ddb12f9656ec82a437873350d214e786c5959
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-09 15:29:07 +0100

    Create FollowersModal

---
+++ src/components/homepage/personalProfile/modals/FollowersModal.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import { useRef, useEffect } from "react";
+export function FollowersModal({ followers, openModal, closeModal }) {
+  const ref = useRef();
+
+  function escapePress(e) {
+    if (e.key === "Escape") closeModal();
+  }
+
+  document.addEventListener("keydown", escapePress);
+
+  useEffect(() => {
+    if (openModal) {
+      ref.current?.openModal();
+    } else {
+      ref.current.close();
+    }
+  });
+
+  return (
+    <dialog ref={ref}>
+      <ul>
+        {followers.map((user) => {
+          <Link to={user.id}>
+            <article>
+              <p>{user.username}</p>
+            </article>
+          </Link>;
+        })}
+      </ul>
+    </dialog>
+  );
+}
