355cb3449b2de3be47a570bf476251ec4a16e98a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-11 14:24:16 +0100

    Add Subtext Color For Dates, LikesCount

--- src/components/homepage/postPreview/PostPreview.jsx
+++ src/components/homepage/postPreview/PostPreview.jsx
@@ -1,14 +1,19 @@
+import styles from "./PostPreview.module.css";
 import { Link } from "react-router-dom";
 export function PostPreview({ post }) {
   return (
     <>
       <Link to={`/posts/${post.id}`}>
-        <article>
-          <img src={post.creator.profilePicURL} alt="" />
-          <p>{post.creator.username}</p>
-          <p>{post.text}</p>
-          <p>{post.datePosted}</p>
-          <p>{post.likesCount}</p>
+        <article className={styles.postPreview}>
+          <img className={styles.profilePic} src={post.creator.profilePicURL} />
+          <div>
+            <div className={styles.topRow}>
+              <span>{post.creator.username}</span>
+              <span className={styles.date}>{post.datePosted}</span>
+            </div>
+            <p>{post.text}</p>
+            <p className={styles.likesCount}>{post.likesCount}</p>
+          </div>
         </article>
       </Link>
     </>
