0657bc9403b476875cad89b6307fbf6c3e4c9c02
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-11 15:21:24 +0100

    Add Different Size Classes For ProfilePic

--- src/components/homepage/profilePic/ProfilePic.jsx
+++ src/components/homepage/profilePic/ProfilePic.jsx
@@ -1,7 +1,19 @@
-export function ProfilePic({ URL }) {
+import styles from "./ProfilePic.module.css";
+
+export function ProfilePic({ URL, component }) {
+  let CLASSNAME = "";
+  if (component === "comment" || component === "post") {
+    CLASSNAME = ".profile-pic-s";
+  } else if (component === "profile") {
+    CLASSNAME = ".profile-pic-l";
+  }
+
   const DEFAULT_ICON_PATH = "../../../assets/user.svg";
-  if (URL === "") return <img src={DEFAULT_ICON_PATH} alt="" />;
-  else {
-    return <img src={URL} alt="" />;
+  if (URL === "") {
+    return (
+      <img className={`${styles}${CLASSNAME}`} src={DEFAULT_ICON_PATH} alt="" />
+    );
+  } else {
+    return <img className={`${styles}${CLASSNAME}`} src={URL} alt="" />;
   }
 }
