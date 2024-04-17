c074d0e57d4d13df0618476f325b9a1e47f25cad
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-17 18:50:44 +0100

    Mock Fetch In BeforeEach Test For Profile Test

--- tests/Profile.test.jsx
+++ tests/Profile.test.jsx
@@ -1,34 +1,39 @@
-// import { describe, it, expect } from "vitest";
-// import { render, screen } from "@testing-library/react";
-// import React from "react";
-// import { createMemoryRouter, Route, RouterProvider } from "react-router-dom";
-// import { PersonalProfile } from "../src/components/homepage/personalProfile/PersonalProfile";
-// import { RenderRouteWithOutletContext } from "./RenderRouteWithOutletContext";
-// import { beforeEach, vi } from "vitest";
+import { describe, it, expect, afterEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { createMemoryRouter, Route, RouterProvider } from "react-router-dom";
+import { PersonalProfile } from "../src/components/homepage/personalProfile/PersonalProfile";
+import { RenderRouteWithOutletContext } from "./RenderRouteWithOutletContext";
+import { beforeEach, vi } from "vitest";
 
-// describe("personal profile", () => {
-//   it("loads settings dialog when clicked", () => {
-//     const loggedInUser = {};
-//     const likedPosts = [];
-//     const routes = [
-//       {
-//         path: "/",
-//         element: (
-//           <RenderRouteWithOutletContext
-//             context={{ loggedInUser, likedPosts }}
-//           />
-//         ),
-//         children: [{ path: "/me", element: <PersonalProfile /> }],
-//       },
-//     ];
-//     const router = createMemoryRouter(routes, { initialEntries: ["/me"] });
-//     render(<RouterProvider router={router} />);
+describe("personal profile", () => {
+  beforeEach(() => {
+    vi.spyOn(global, "fetch").mockImplementation(() => {
+      return;
+    });
+  });
 
-//     vi.spyOn(React, "useEffect").mockImplementation(() => {
-//       const isLoading = false;
-//       return isLoading;
-//     });
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
 
-//     expect(screen.getByRole("span", { name: "settings" })).toBeInTheDocument();
-//   });
-// });
+  it("loads settings dialog when clicked", () => {
+    const loggedInUser = {};
+    const likedPosts = [];
+    const routes = [
+      {
+        path: "/",
+        element: (
+          <RenderRouteWithOutletContext
+            context={{ loggedInUser, likedPosts }}
+          />
+        ),
+        children: [{ path: "/me", element: <PersonalProfile /> }],
+      },
+    ];
+    const router = createMemoryRouter(routes, { initialEntries: ["/me"] });
+    render(<RouterProvider router={router} />);
+
+    expect(screen.getByRole("span", { name: "settings" })).toBeInTheDocument();
+  });
+});
