4b7fcf286a4c8854bbaaa15d7c4d089e64e26c0a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-23 18:49:30 +0100

    Add UseOutletContext To SearchUsersBar

--- src/components/homepage/header/SearchUsersBar.jsx
+++ src/components/homepage/header/SearchUsersBar.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useOutletContext } from "react-router-dom";
 
 export function SearchUsersBar() {
   const { searchQuery, setSearchQuery, setMatchingUsers } = useOutletContext();
