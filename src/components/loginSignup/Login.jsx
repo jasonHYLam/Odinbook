77790960bf3468e54f7219414fb7c496d31e43b4
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-23 17:00:26 +0100

    Round Login And Signup Button

--- src/components/loginSignup/Login.jsx
+++ src/components/loginSignup/Login.jsx
@@ -39,43 +39,52 @@ export function Login() {
   }
 
   return (
-    <main className={styles.page}>
-      <section className={styles.logoAndForm}>
-        <div className={styles.logo}>
-          <Logo size="large" />
-        </div>
-        <form className={styles.form} onSubmit={handleSubmit(submitLoginData)}>
-          <h1>Tabasco</h1>
-          <h2>Login</h2>
-          <input
-            className={styles.input}
-            type="text"
-            {...register("username", { required: true })}
-            placeholder="Username"
-          />
-          <input
-            className={styles.input}
-            type="password"
-            {...register("password", { required: true })}
-            placeholder="Password"
-          />
-          <input type="submit" value={SUBMIT_BUTTON_TEXT} />
-          <section className={styles.errorsList}>
-            {errors.username && (
-              <span className={styles.error}>Please provide username</span>
-            )}
-            {errors.password && (
-              <span className={styles.error}>Please provide password</span>
-            )}
-            {authError ? (
-              <span className={styles.error}>{authError}</span>
-            ) : null}
-          </section>
-          <GuestLoginLink />
-          <Link className={styles.link} to="/sign_up">
-            <span>New here? Sign up</span>
-          </Link>
-        </form>
+    <main>
+      <section className={styles.page}>
+        <section className={styles.logoAndForm}>
+          <div className={styles.logo}>
+            <Logo size="large" />
+          </div>
+          <form
+            className={styles.form}
+            onSubmit={handleSubmit(submitLoginData)}
+          >
+            <h1>Tabasco</h1>
+            <h2>Login</h2>
+            <input
+              className={styles.input}
+              type="text"
+              {...register("username", { required: true })}
+              placeholder="Username"
+            />
+            <input
+              className={styles.input}
+              type="password"
+              {...register("password", { required: true })}
+              placeholder="Password"
+            />
+            <input
+              className={styles.submit}
+              type="submit"
+              value={SUBMIT_BUTTON_TEXT}
+            />
+            <section className={styles.errorsList}>
+              {errors.username && (
+                <span className={styles.error}>Please provide username</span>
+              )}
+              {errors.password && (
+                <span className={styles.error}>Please provide password</span>
+              )}
+              {authError ? (
+                <span className={styles.error}>{authError}</span>
+              ) : null}
+            </section>
+            <GuestLoginLink />
+            <Link className={styles.link} to="/sign_up">
+              <span>New here? Sign up</span>
+            </Link>
+          </form>
+        </section>
       </section>
     </main>
   );
--- src/components/loginSignup/Signup.jsx
+++ src/components/loginSignup/Signup.jsx
@@ -40,93 +40,103 @@ export function Signup() {
 
   return (
     <main className={styles.page}>
-      <Logo size="large" />
-      <p>Signup</p>
-      <form className={styles.form} onSubmit={handleSubmit(submitSignupData)}>
-        <input
-          type="text"
-          placeholder="Username"
-          {...register("username", {
-            required: true,
-            minLength: USERNAME_MIN,
-            maxLength: USERNAME_MAX,
-            onChange: () => {
-              if (usernameError !== "") setUsernameError("");
-            },
-          })}
-        />
+      <section className={styles.logoAndForm}>
+        <div className={styles.logo}>
+          <Logo size="large" />
+        </div>
+        <form className={styles.form} onSubmit={handleSubmit(submitSignupData)}>
+          <h1>Tabasco</h1>
+          <h2>Signup</h2>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Username"
+            {...register("username", {
+              required: true,
+              minLength: USERNAME_MIN,
+              maxLength: USERNAME_MAX,
+              onChange: () => {
+                if (usernameError !== "") setUsernameError("");
+              },
+            })}
+          />
 
-        <input
-          type="password"
-          placeholder="Password"
-          {...register("password", {
-            required: true,
-            minLength: PASSWORD_MIN,
-            onChange: () => {
-              if (usernameError !== "") setUsernameError("");
-            },
-          })}
-        />
+          <input
+            className={styles.input}
+            type="password"
+            placeholder="Password"
+            {...register("password", {
+              required: true,
+              minLength: PASSWORD_MIN,
+              onChange: () => {
+                if (usernameError !== "") setUsernameError("");
+              },
+            })}
+          />
 
-        <input
-          type="password"
-          placeholder="Confirm password"
-          {...register("confirmPassword", {
-            required: true,
-            validate: (val) => {
-              if (getValues("password") !== val) {
-                return "Passwords don't match";
-              }
-            },
-            onChange: () => {
-              if (usernameError !== "") setUsernameError("");
-            },
-          })}
-        />
+          <input
+            className={styles.input}
+            type="password"
+            placeholder="Confirm password"
+            {...register("confirmPassword", {
+              required: true,
+              validate: (val) => {
+                if (getValues("password") !== val) {
+                  return "Passwords don't match";
+                }
+              },
+              onChange: () => {
+                if (usernameError !== "") setUsernameError("");
+              },
+            })}
+          />
 
-        <input type="submit" value="Sign up" />
-        <section className={styles.errorsList}>
-          {errors.username && errors.username.type === "required" && (
-            <span className={styles.error}>Please provide username</span>
-          )}
-
-          {errors.username && errors.username.type === "minLength" && (
-            <span className={styles.error}>
-              Username must be at least {USERNAME_MIN} characters
-            </span>
-          )}
-
-          {errors.username && errors.username.type === "maxLength" && (
-            <span className={styles.error}>
-              Username must be less than {USERNAME_MAX} characters
-            </span>
-          )}
-
-          {errors.confirmPassword &&
-            errors.confirmPassword.type === "required" && (
-              <span className={styles.error}>Please confirm password</span>
+          <input className={styles.submit} type="submit" value="Sign up" />
+          <section className={styles.errorsList}>
+            {errors.username && errors.username.type === "required" && (
+              <span className={styles.error}>Please provide username</span>
             )}
 
-          {errors.password && errors.password.type === "minLength" && (
-            <span className={styles.error}>
-              Password must be at least {PASSWORD_MIN} characters{" "}
-            </span>
-          )}
-
-          {errors.confirmPassword &&
-            errors.confirmPassword.type === "validate" && (
+            {errors.username && errors.username.type === "minLength" && (
               <span className={styles.error}>
-                {errors.confirmPassword.message}
+                Username must be at least {USERNAME_MIN} characters
               </span>
             )}
 
-          {usernameError !== "" ? (
-            <span className={styles.error}>usernameError</span>
-          ) : null}
-        </section>
-      </form>
-      <GuestLoginLink />
-      <Link to="/login">Got an account? Login</Link>
+            {errors.username && errors.username.type === "maxLength" && (
+              <span className={styles.error}>
+                Username must be less than {USERNAME_MAX} characters
+              </span>
+            )}
+
+            {errors.confirmPassword &&
+              errors.confirmPassword.type === "required" && (
+                <span className={styles.error}>Please confirm password</span>
+              )}
+
+            {errors.password && errors.password.type === "minLength" && (
+              <span className={styles.error}>
+                Password must be at least {PASSWORD_MIN} characters{" "}
+              </span>
+            )}
+
+            {errors.confirmPassword &&
+              errors.confirmPassword.type === "validate" && (
+                <span className={styles.error}>
+                  {errors.confirmPassword.message}
+                </span>
+              )}
+
+            {usernameError !== "" ? (
+              <span className={styles.error}>usernameError</span>
+            ) : null}
+          </section>
+          <GuestLoginLink />
+          <Link to="/login">
+            <span className={styles.link}>Got an account? Login</span>
+          </Link>
+        </form>
+      </section>
     </main>
   );
 }
