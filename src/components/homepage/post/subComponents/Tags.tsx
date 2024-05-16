b2ed441f8341bc4e429295dffaac76ddf7a9d31b
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-16 22:29:28 +0100

    Convert Tags component to Typescript

--- src/components/homepage/post/subComponents/Tags.tsx
+++ src/components/homepage/post/subComponents/Tags.tsx
@@ -1,5 +1,15 @@
 import styles from "./tags.module.css";
-export function Tags({ tags }) {
+
+interface Tag {
+  id: string;
+  name: string;
+}
+
+interface TagsProps {
+  tags: Tag[];
+}
+
+export function Tags({ tags }: TagsProps) {
   return tags.map((tag) => (
     <span className={styles.tag} key={tag.id}>{`#${tag.name}`}</span>
   ));
