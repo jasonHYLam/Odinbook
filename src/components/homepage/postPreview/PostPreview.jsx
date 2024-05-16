03af79af99f300020bcdd628728b6d9c4e0bcfa0
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-16 15:55:08 +0100

    Add path to user profiles for feed

--- src/components/homepage/postPreview/PostPreview.jsx
+++ src/components/homepage/postPreview/PostPreview.jsx
@@ -1,27 +1,36 @@
 import styles from "./PostPreview.module.css";
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 import { ProfilePic } from "../icons/profilePic/ProfilePic";
 
 // create default thumbnail if doesn't exist
 // create some stuff here i guess...
 // check properties of post in backend
 export function PostPreview({ post }) {
+  const { loggedInUser } = useOutletContext();
+
+  const userID =
+    loggedInUser.id === post.creator.id ? loggedInUser.id : post.creator.id;
+
   return (
     <>
-      <Link to={`/posts/${post.id}`}>
-        <article className={styles.postPreview}>
-          <div>
+      <article className={styles.postPreview}>
+        <div>
+          <Link to={`/posts/${post.id}`}>
             {post.thumbnailImageURL ? (
               <img className={styles.img} src={post.thumbnailImageURL} />
             ) : null}
-            <p className={styles.text}>{post.title}</p>
+          </Link>
+          <Link to={`/posts/${post.id}`}>
+            <p className={styles.title}>{post.title}</p>
+          </Link>
+          <Link to={`/users/${userID}`}>
             <div className={styles.topRow}>
               <ProfilePic URL={post.creator.profilePicURL} size="small" />
               <span className={styles.username}>{post.creator.username}</span>
             </div>
-          </div>
-        </article>
-      </Link>
+          </Link>
+        </div>
+      </article>
     </>
   );
 }
