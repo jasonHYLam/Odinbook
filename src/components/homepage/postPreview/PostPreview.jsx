e9cddbefa058f59d5051b6e69160ab7df57d2cf3
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-16 13:08:19 +0100

    Create postsContainer

--- src/components/homepage/postPreview/PostPreview.jsx
+++ src/components/homepage/postPreview/PostPreview.jsx
@@ -2,6 +2,9 @@ import styles from "./PostPreview.module.css";
 import { Link } from "react-router-dom";
 import { ProfilePic } from "../icons/profilePic/ProfilePic";
 
+// create default thumbnail if doesn't exist
+// create some stuff here i guess...
+// check properties of post in backend
 export function PostPreview({ post }) {
   return (
     <>
@@ -14,10 +17,12 @@ export function PostPreview({ post }) {
               <span className={styles.date}>{post.datePostedFormatted}</span>
             </div>
 
-            {post.imageURL ? (
-              <img className={styles.img} src={post.imageURL} />
+            {post.thumbnailImageURL ? (
+              // <img className={styles.img} src={post.imageURL} />
+              <img className={styles.img} src={post.thumbnailImageURL} />
             ) : null}
-            <p className={styles.text}>{post.text}</p>
+            {/* <p className={styles.text}>{post.text}</p> */}
+            <p className={styles.text}>{post.title}</p>
             <p className={styles.likesCount}>{post.likesCount}</p>
           </div>
         </article>
