73e9c63cd751b75bb7da1b6795b8a018176a62f6
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-19 15:18:59 +0100

    Migrate PostsContainer to ts

--- src/components/homepage/postsContainer/PostsContainer.tsx
+++ src/components/homepage/postsContainer/PostsContainer.tsx
@@ -1,7 +1,12 @@
+import { PostType } from "../../../helper/types";
 import { PostPreview } from "../postPreview/PostPreview";
 import styles from "./postsContainer.module.css";
 
-export function PostsContainer({ posts }) {
+interface PostsContainerProps {
+  posts: PostType[];
+}
+
+export function PostsContainer({ posts }: PostsContainerProps) {
   return (
     <section className={styles.postsContainer}>
       {posts.map((post) => (
