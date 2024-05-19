66e1edf276fd528710cfb2e395b98e74521a6080
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-05-19 20:35:55 +0100

    Migrate Header component to ts

--- src/components/homepage/header/Header.tsx
+++ src/components/homepage/header/Header.tsx
@@ -1,15 +1,23 @@
-import { Link, useOutletContext } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Logo } from "../icons/logo/Logo";
 import { ProfilePic } from "../icons/profilePic/ProfilePic";
 import styles from "./Header.module.css";
 import { SearchUsersBar } from "./SearchUsersBar";
+import { UserType } from "../../../helper/types";
+
+interface HeaderProps {
+  user: UserType;
+  searchQuery: string;
+  setSearchQuery: (searchQuery: string) => void;
+  setMatchingUsers: (matchingUsers: UserType[]) => void;
+}
 
 export function Header({
   user,
   searchQuery,
   setSearchQuery,
   setMatchingUsers,
-}) {
+}: HeaderProps) {
   return (
     <header>
       <Link to="/">
